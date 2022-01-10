1eaf09aa42e152a01ebb40d4016b78e96e9b955c
Sean Gillespie <seene3@gmail.com>
2022-01-09 19:17:57 -0500

    set up library functions

---
+++ index.js
@@ -0,0 +1,29 @@
+const Employee = require('../lib/Employee.js');
+const Engineer = require('../lib/Engineer.js');
+const Manager = require('../lib/Manager.js');
+const Intern = require('../lib/Intern.js');
+
+class Profile {
+    constructor() {
+        this.Employee;
+        this.Engineer;
+        this.Manager;
+        this.Intern;
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+
+    }
+}
